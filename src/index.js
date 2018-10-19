69d902f1d8904e6463051938e37313bacc4f8d4f
DL <appbucketrack@gmail.com>
2018-10-19 16:19:51 +1100

    babel presents

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('hello git');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const el = document.getElementById('root');
+
+function App() {
+	return(
+		<div className="App">
+			<h1>App</h1>
+		</div>
+	);
+}
+
+ReactDOM.render(<App />, el);
